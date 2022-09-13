ce21e221dbf1cc8de11d30b9c2c6ff9a0105cfcf
Kevin Bell <bellKevin@pm.me>
2022-09-13 13:29:42 -0600

    Update MilesPerGallonRevisited.java

--- milesPerGallonRevisited/src/milespergallonrevisited/MilesPerGallonRevisited.java
+++ milesPerGallonRevisited/src/milespergallonrevisited/MilesPerGallonRevisited.java
@@ -14,13 +14,14 @@ public class MilesPerGallonRevisited {
     public static void main(String[] args) {
         System.out.println("Ch 15 MPG Calculator by Kevin Bell \n");
         ArrayList<MPG> milesPerGallon = new ArrayList<>();
-        milesPerGallon.add(new MPG(55.52, 21.30));
+        //milesPerGallon.add(new MPG(55.52, 21.30));
         for (MPG milesPG : milesPerGallon) {
             MPG.displayTotalMPG();
         } // end for-each loop
         System.out.println("");
         boolean inputValid = false;
         String userInput = "";
+        double miles = 0, mileage, gallons = 0;
         do {
             System.out.print("Select (n)New trip, (d)Display total MPG, "
                     + "(q)Quit: ");
@@ -33,7 +34,27 @@ public class MilesPerGallonRevisited {
                 System.out.println("bad input, try again");
             } // end if/else statement
         } while (!inputValid); // end do/while loop
-        // look at used car auto lot driver for the next step
-        // i believe the next step is a for-each loop with switch cases
+        while (!userInput.equalsIgnoreCase("q")) {
+            try {
+                System.out.print("Select (n)New trip, (d)Display total MPG, "
+                        + "(q)Quit: ");
+                Scanner computerKeyboardInput = new Scanner(System.in);
+                userInput = computerKeyboardInput.next();
+                System.out.print("Enter miles: ");
+                userInput = computerKeyboardInput.next();
+                miles = Double.parseDouble(userInput);
+                System.out.print("Enter gallons: ");
+                userInput = computerKeyboardInput.next();
+                gallons = Double.parseDouble(userInput);
+                if (gallons == 0 && miles == 0) {
+                    System.out.println("There are no trips yet");
+                } // end if condition
+                MPG.displayCurrentMPG();
+            } catch (NumberFormatException nfe) {
+                System.out.println("Bad input, try again");
+            } // end try/catch exception handling
+            // look at used car auto lot driver for the next step
+            // i believe the next step is a for-each loop with switch cases
+        } // end while loop
     } // end main method
 } // end MilesPerGallonRevisited class
