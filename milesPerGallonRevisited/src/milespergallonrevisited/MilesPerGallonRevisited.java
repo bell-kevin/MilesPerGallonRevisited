68242f0dbafccb9861ec771bc0b911067c93834d
Kevin Bell <bellKevin@pm.me>
2022-09-12 18:08:03 -0600

    Add files via upload

---
+++ milesPerGallonRevisited/src/milespergallonrevisited/MilesPerGallonRevisited.java
@@ -0,0 +1,14 @@
+/** ****************************************************************************
+ * MilesPerGallonRevisited.java
+ * Kevin Bell
+ *
+ * Driver calculates miles per gallon for a single trip and for cumulative trips
+ **************************************************************************** */
+package milespergallonrevisited;
+
+public class MilesPerGallonRevisited {
+
+    public static void main(String[] args) {
+        
+    } // end main method
+} // end MilesPerGallonRevisited class
